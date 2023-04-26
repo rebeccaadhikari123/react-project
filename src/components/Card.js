082d1e1dbdb230e4b607250fc32b3d2dd6e515a7
rebeccaadhikari123 <85625859+rebeccaadhikari123@users.noreply.github.com>
2023-04-26 13:13:14 +0545

    changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,11 +12,13 @@ function NewsCard({ data })
     <Card style={{ width: '20rem',margin: '1.2rem'}}>
       <Card.Img variant="top" src={data.urlToImage} />
       <div className="alert alert-info" role="alert">
-      <Card.Body>
+      <Card.Body >
         <Card.Title>{data.title}</Card.Title>
-        <Card.Text>
+        <Card.Text >
          {data.description}
+
         </Card.Text>
+        
         <Button a href={data.url} variant="light" target=" ">{data.source.name}  </Button> 
          {/*<a href="data.url" className="btn btn-success">{data.url}</a> */}
       </Card.Body>
